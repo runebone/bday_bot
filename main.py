26963c0d217b481e000c8e48d3699e23b9a1e32e
Konstantin Runov <runebone1@gmail.com>
2022-04-17 18:28:34 +0300

    backup; draft version; basic functionality

---
+++ main.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+import telebot
+from my_regex import *
+
+# TODO: Сделать клаву с командами, чтобы бабки могли пользоваться
+
+# Get API token
+with open(".API_TOKEN") as f:
+    API_TOKEN = f.readline()[:-1]
+
+bot = telebot.TeleBot(API_TOKEN)
+# config.py ===============================================
+from enum import Enum
+
+class BotText(Enum):
+    START = "Start message example."
+    ADD = "Add message example."
+    SHOW = "Show message example."
+    DELETE = "Delete message example."
+    EDIT = "Edit message example."
+
+# error.py ===============================================
+class Error(Exception):
+    """Base class for custom exceptions."""
+    pass
+
+class MessageTooLarge(Error):
+    text = "Message is too large."
+class NoNameInTheBeginning(Error):
+    text = "Message should start with name."
+class NoDate(Error):
+    text = "Message should contain date."
+class InvalidNickname(Error):
+    text = "Nickname should be from 5 to 32 characters long."
+
+# const.py ===============================================
+class Const: # TODO: from const.py import *; remove Const class
+    MAX_MESSAGE_LENGTH = 256
+
+# ===============================================
+
+@bot.message_handler(commands=["start", "help"])
+def start(message):
+    bot.send_message(message.chat.id, BotText.START.value)
+
+@bot.message_handler(commands=["add"])
+def add(message):
+    bot.send_message(message.chat.id, BotText.ADD.value)
+    #bot.register_next_step_handler(message, process_add_step)
+
+@bot.message_handler(commands=["delete", "cut"])
+def delete(message):
+    bot.send_message(message.chat.id, BotText.DELETE.value)
+    #bot.register_next_step_handler(message, process_delete_step)
+
+@bot.message_handler(commands=["show", "see"])
+def show(message):
+    bot.send_message(message.chat.id, BotText.SHOW.value)
+    #bot.register_next_step_handler(message, process_show_step)
+
+@bot.message_handler(commands=["edit", "change"])
+def edit(message):
+    bot.send_message(message.chat.id, BotText.EDIT.value)
+    #bot.register_next_step_handler(message, process_edit_step)
+
+# === PROCESS_ADD_STEP
+def process_add_step(message):
+    try:
+        assert_message_has_valid_length(message)
+        assert_message_has_name_in_the_beginning(message)
+        assert_message_has_date(message)
+
+        if (message_has_at_sign(message)):
+            if (message_has_nickname(message)):
+                # nickname -> database
+            else:
+                raise InvalidNickname
+
+        if (message_has_phone(message)):
+            # phone -> normalize -> beautify -> database
+
+    except Exception as e:
+        bot.send_message(message.chat.id, e.text)
+
+def assert_message_has_valid_length(message):
+    if (len(message.text) > Const.MAX_MESSAGE_LENGTH):
+        raise MessageTooLarge
+
+def assert_message_has_name_in_the_beginning(message):
+    name = get_name_from_message(message.text)
+    if (name == None):
+        raise NoNameInTheBeginning
+
+def assert_message_has_date(message):
+    date = get_date_from_message(message.text)
+    if (date == None):
+        raise NoDate
+
+def message_has_at_sign(message):
+    if (re.findall("@", message.text) == []):
+        return False
+    return True
+
+def message_has_nickname(message):
+    nickname = get_nickname_from_message(message.text)
+    if (nickname == None):
+        return False
+    return True
+
+def message_has_phone(message):
+    phone = get_phone_from_message(message.text)
+    if (phone == None):
+        return False
+    return True
+# === END OF PROCESS_ADD_STEP
+
+def process_delete_step(message):
+    pass
+
+def process_show_step(message):
+    pass
+
+def process_edit_step(message):
+    pass
+
+# ===============================================
+bot.polling() # Keeps checking for messages
