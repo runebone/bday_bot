d11b97ddc540220642d6c8b1c13f651562d84ffd
Konstantin Runov <runebone1@gmail.com>
2022-04-18 07:59:43 +0300

    ADD: init config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class BotText(Enum):
+    START = "Start message example."
+    ADD = "Add message example."
+    SHOW = "Show message example."
+    DELETE = "Delete message example."
+    EDIT = "Edit message example."
