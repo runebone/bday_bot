8ff28e4f82a3df4706a916e2fa57adeba419c1b8
Konstantin Runov <runebone1@gmail.com>
2022-04-18 09:53:25 +0300

    db: add delete func; many func and var renames

--- db.py
+++ db.py
@@ -4,6 +4,8 @@ from errors import *
 
 file = "database.json"
 
+# XXX: Date is stored in MM-DD-YYYY (USA) format.
+
 record_fields = list({
     "name": None,
     "date": None,
@@ -28,54 +30,69 @@ class Database:
         return database_dict
 
     def dump(self, updated_database_dict):
-        with open(self.file, "r+") as f:
+        with open(self.file, "w") as f:
             json.dump(updated_database_dict, f, indent=4)
 
     def assert_valid_record_fields(self, record_dict):
         if (list(record_dict.keys()) != self.record_fields):
             raise InvalidRecordFields
 
-    def get_users_list(self, database_dict):
-        users = list(database_dict[self.users][0].keys())
-        return users
+    def get_users_list_from_dict(self, database_dict):
+        users_list = list(database_dict[self.users][0].keys())
+        return users_list
 
-    def add_user_record(self, database_dict, chat_id_str, record_dict):
+    def add_user_record_to_dict(self, database_dict, chat_id_str, record_dict):
         if (record_dict not in database_dict[self.users][0][chat_id_str]):
             database_dict[self.users][0][chat_id_str].append(record_dict)
 
-    def add_new_user(self, database_dict, chat_id_str):
+    def add_new_user_to_dict(self, database_dict, chat_id_str):
         database_dict[self.users][0][chat_id_str] = []
 
-    def save_new_record(self, chat_id, record_dict):
+    def add_new_record(self, chat_id, record_dict):
         self.assert_valid_record_fields(record_dict)
 
         chat_id = str(chat_id)
         database = self.load()
-        users = self.get_users_list(database)
+        users = self.get_users_list_from_dict(database)
 
         if (chat_id in users):
-            self.add_user_record(database, chat_id, record_dict)
+            self.add_user_record_to_dict(database, chat_id, record_dict)
         else:
-            self.add_new_user(database, chat_id)
-            self.add_user_record(database, chat_id, record_dict)
+            self.add_new_user_to_dict(database, chat_id)
+            self.add_user_record_to_dict(database, chat_id, record_dict)
 
         self.dump(database)
 
-    def get_user_records(self, database_dict, chat_id_str):
+    def get_user_records_from_dict(self, database_dict, chat_id_str):
         return database_dict[self.users][0][chat_id_str]
+
+    def get_user_records(self, chat_id):
+        chat_id = str(chat_id)
+        database = self.load()
+        user_records = self.get_user_records_from_dict(database, chat_id)
+
+        return user_records
 
     def delete_record_by_index(self, chat_id, record_index):
         chat_id = str(chat_id)
         database = self.load()
-        users = self.get_users_list(database)
+        users = self.get_users_list_from_dict(database)
 
         if (chat_id not in users):
             raise NewUserHasNoRecords
-        elif (self.get_user_records(database, chat_id) == []):
+        elif (self.get_user_records_from_dict(database, chat_id) == []):
             raise UserHasNoRecords
         else:
-            records_df = pd.DataFrame(self.get_user_records(database, chat_id))
-            print(records_df)
+            # df - user records dataframe
+            df = pd.DataFrame(self.get_user_records_from_dict(database, \
+                                                                chat_id))
+            df = df.sort_values(by="date").reset_index(drop=True)
+            df = df.drop(labels=record_index, axis=0) # Delete record
+            df = df.to_dict(orient="records")
+
+            database[self.users][0][chat_id] = df
+
+            self.dump(database)
 
     # TODO: sort by field without loading. sort by table keys (indexes).
     # TODO: load single record without loading whole db.
@@ -84,11 +101,13 @@ if __name__ == "__main__":
     db = Database()
 
     record = {
-        "name": "human",
+        "name": "humanoid",
         "date": "12.12.1212",
         "nickname": None,
         "phone": "lskj",
         "note": None
     }
 
-    db.delete_record_by_index(317823738, "asdf")
+    #db.add_new_record(317823738, record)
+    #db.delete_record_by_index(317823738, 1)
+    print(db.get_user_records(317823738))
