6a4d2c6455dd2d33baa484fe74a0a8ed60975a07
Konstantin Runov <runebone1@gmail.com>
2022-04-19 11:49:38 +0300

    delete: sing. imp; func. rename; tb; rm useless

--- bot_commands/delete.py
+++ bot_commands/delete.py
@@ -1,6 +1,4 @@
-from config import *
-from bot_commands.show import get_printable_string_from_record
-from bot_commands.edit import gen_edit_markup
+from bot_commands.common import *
 
 def process_delete_step(message, bot, db):
     try:
@@ -8,7 +6,7 @@ def process_delete_step(message, bot, db):
         # TODO: get index from user
         records = db.get_user_records(message.chat.id)
         for i in range(len(records)):
-            string = get_printable_string_from_record(records[i], i)
+            string = get_output_string(records[i], i)
             bot.send_message(message.chat.id, string, \
                     reply_markup=gen_edit_markup())
 
@@ -19,8 +17,8 @@ def process_delete_step(message, bot, db):
     except NewUserHasNoRecords:
         bot.send_message(message.chat.id, FailText.NewUserHasNoRecords)
         bot.register_next_step_handler(message, process_delete_step, bot, db)
-    except RecordIndexOutOfRange:
-        bot.send_message(message.chat.id, FailText.RecordIndexOutOfRange)
-        bot.register_next_step_handler(message, process_delete_step, bot, db)
     except Exception as e:
-        bot.send_message(message.chat.id, FailText.UncaughtError.format(str(e)))
+        bot.send_message(message.chat.id, \
+                FailText.UncaughtError.format(str(e)))
+        tb = sys.exc_info()[2]
+        raise e.with_traceback(tb)
