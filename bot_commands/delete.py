160ba69be9925fe643f51decf5afa8cf1e2760c5
Konstantin Runov <runebone1@gmail.com>
2022-04-19 14:38:07 +0300

    delete: assert; bugfix

--- bot_commands/delete.py
+++ bot_commands/delete.py
@@ -2,7 +2,8 @@ from bot_commands.common import *
 
 def process_delete_step(message, bot, db):
     try:
-        # TODO: some asserts (user has records)
+        assert_user_has_records(message, db)
+
         # TODO: get index from user
         records = db.get_user_records(message.chat.id)
         for i in range(len(records)):
@@ -13,10 +14,8 @@ def process_delete_step(message, bot, db):
     # FIXME: DRY
     except UserHasNoRecords:
         bot.send_message(message.chat.id, FailText.UserHasNoRecords)
-        bot.register_next_step_handler(message, process_delete_step, bot, db)
     except NewUserHasNoRecords:
         bot.send_message(message.chat.id, FailText.NewUserHasNoRecords)
-        bot.register_next_step_handler(message, process_delete_step, bot, db)
     except Exception as e:
         bot.send_message(message.chat.id, \
                 FailText.UncaughtError.format(str(e)))
