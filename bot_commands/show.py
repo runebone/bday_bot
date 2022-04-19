f5f85cb0578b7f9a4bb233d4e23a3a158183fa42
Konstantin Runov <runebone1@gmail.com>
2022-04-19 13:30:28 +0300

    show: assert user has records

--- bot_commands/show.py
+++ bot_commands/show.py
@@ -2,8 +2,10 @@ from bot_commands.common import *
 
 def process_show_step(message, bot, db):
     try:
-        # TODO: some asserts (user has records)
+        assert_user_has_records(message, db)
+
         records = db.get_user_records(message.chat.id)
+
         for i in range(len(records)):
             string = get_output_string(records[i], i)
             bot.send_message(message.chat.id, string)
@@ -11,13 +13,14 @@ def process_show_step(message, bot, db):
     # FIXME: DRY
     except UserHasNoRecords:
         bot.send_message(message.chat.id, FailText.UserHasNoRecords)
-        bot.register_next_step_handler(message, process_show_step, bot, db)
-    except NewUserHasNoRecords:
-        bot.send_message(message.chat.id, FailText.NewUserHasNoRecords)
-        bot.register_next_step_handler(message, process_show_step, bot, db)
     except Exception as e:
         bot.send_message(message.chat.id, \
                 FailText.UncaughtError.format(str(e)))
 
         tb = sys.exc_info()[2]
         raise e.with_traceback(tb)
+
+def assert_user_has_records(message, db):
+    records = db.get_user_records(message.chat.id)
+    if (len(records) == 0):
+        raise UserHasNoRecords
