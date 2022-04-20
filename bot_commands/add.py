7ea5a13d9b68ba1e24ae8f9da37a2f989320a26b
Konstantin Runov <runebone1@gmail.com>
2022-04-20 18:08:03 +0300

    add: fix dry in try except

--- bot_commands/add.py
+++ bot_commands/add.py
@@ -14,24 +14,17 @@ def process_add_step(message, bot, db):
         bot.send_message(message.chat.id, BotText.ADD_SUCCESS, \
                 reply_markup=gen_default_actions_markup())
 
-    # FIXME: DRY
     except MessageIsCommand:
-        bot.send_message(message.chat.id, "Вы вышли из режима добавления. Введите команду {} повторно, чтобы исполнить её.".format(message.text))
-        default(message, bot)
+        MessageIsCommand.add(message, default, bot, db)
     except MessageTooLarge:
-        bot.send_message(message.chat.id, FailText.MessageTooLarge)
-        bot.register_next_step_handler(message, process_add_step, bot, db)
+        MessageTooLarge.default(message, process_add_step, bot, db)
     except NoNameInTheBeginning:
-        bot.send_message(message.chat.id, FailText.NoNameInTheBeginning)
-        bot.register_next_step_handler(message, process_add_step, bot, db)
+        NoNameInTheBeginning.default(message, process_add_step, bot, db)
     except NoDate:
-        bot.send_message(message.chat.id, FailText.NoDate)
-        bot.register_next_step_handler(message, process_add_step, bot, db)
+        NoDate.default(message, process_add_step, bot, db)
     except InvalidNickname:
-        bot.send_message(message.chat.id, FailText.InvalidNickname)
-        bot.register_next_step_handler(message, process_add_step, bot, db)
+        InvalidNickname.default(message, process_add_step, bot, db)
     except RecordAlreadyExists:
-        bot.send_message(message.chat.id, FailText.RecordAlreadyExists)
-        bot.register_next_step_handler(message, process_add_step, bot, db)
+        RecordAlreadyExists.default(message, process_add_step, bot, db)
     except Exception as e:
         uncaught_error(message, bot, e)
