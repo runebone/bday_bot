a9c187bd13bf9e890fa7fafc68b6789f53e2d1d2
Konstantin Runov <runebone1@gmail.com>
2022-04-20 09:41:07 +0300

    db: get_record_index_by_record

--- db.py
+++ db.py
@@ -119,6 +119,27 @@ class Database:
                 database[self.users][0][chat_id].remove(record)
                 self.dump(database)
 
+    def get_record_index_by_record(self, chat_id, record):
+        index = None
+        chat_id = str(chat_id)
+        database = self.load()
+        users = self.get_users_list_from_dict(database)
+
+        if (chat_id not in users):
+            raise NewUserHasNoRecords
+        else:
+            user_records = self.get_user_records_from_dict(database, chat_id)
+
+        if (user_records == []):
+            raise UserHasNoRecords
+        else:
+            if (record not in database[self.users][0][chat_id]):
+                raise RecordNotFound
+            else:
+                index = database[self.users][0][chat_id].index(record)
+
+        return index
+
     def get_record_by_index(self, chat_id, record_index):
         chat_id = str(chat_id)
         database = self.load()
