f00c9f4886d503bda257a87f3f02ea56936659c4
Konstantin Runov <runebone1@gmail.com>
2022-04-20 08:14:19 +0300

    __init__: delete, edit -> edit_delete

--- bot_commands/__init__.py
+++ bot_commands/__init__.py
@@ -1,5 +1,4 @@
 import bot_commands.add as add
-import bot_commands.delete as delete
+import bot_commands.edit_delete as edit_delete
 import bot_commands.show as show
-import bot_commands.edit as edit
 import bot_commands.common as common
