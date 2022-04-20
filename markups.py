ab8ca7e18bdecc7f6e74c6fe61af27850c181584
Konstantin Runov <runebone1@gmail.com>
2022-04-20 08:03:26 +0300

    markups: edit record markup; beautify code

--- markups.py
+++ markups.py
@@ -1,47 +1,86 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot_commands.common import *
 
-def gen_edit_record_markup():
+def gen_choose_edit_delete_record_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    markup.add(InlineKeyboardButton("Изменить", \
-            callback_data="cb_edit_record"), \
-            InlineKeyboardButton("Удалить", \
-            callback_data="cb_delete_record"))
+
+    IKB = InlineKeyboardButton
+
+    edit = IKB("Изменить", callback_data="cb_edit_record")
+    delete = IKB("Удалить", callback_data="cb_delete_record")
+
+    markup.add(edit, delete)
+
     return markup
 
+def gen_edit_record_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 4
+
+    IKB = InlineKeyboardButton
+
+    name = IKB("Имя", callback_data="cb_edit_name")
+    date = IKB("Дата", callback_data="cb_edit_date")
+    nickname = IKB("Никнейм", callback_data="cb_edit_nickname")
+    phone = IKB("Телефон", callback_data="cb_phone_name")
+    cancel = IKB("Отмена", callback_data="cb_cancel")
+    input_again = IKB("Ввести заново", callback_data="cb_input_again")
+
+    markup.add(name, date, nickname, phone, cancel, input_again)
+
+    return markup
+
+# TODO: Button names -> config
 def gen_add_friend_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
-    markup.add(InlineKeyboardButton("Добавить друга", \
-            callback_data="cb_add_command"))
+
+    IKB = InlineKeyboardButton
+
+    add_friend = IKB("Добавить друга", callback_data="cb_add_command")
+
+    markup.add(add_friend)
+
     return markup
 
 def gen_default_actions_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    markup.add(InlineKeyboardButton("Добавить друга", \
-                callback_data="cb_add_command"),
-            InlineKeyboardButton("Посмотреть список",
-                callback_data="cb_show_command"),
-            InlineKeyboardButton("Изменить / Удалить",
-                callback_data="cb_delete_command"))
+
+    IKB = InlineKeyboardButton
+
+    add_friend = IKB("Добавить друга", callback_data="cb_add_command")
+    show = IKB("Посмотреть список", callback_data="cb_show_command")
+    # TODO: rename delete callback command
+    edit_delete = IKB("Изменить / Удалить", callback_data="cb_delete_command")
+
+    markup.add(add_friend, show, edit_delete)
+
     return markup
 
 def gen_confirm_deletion_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    markup.add(InlineKeyboardButton("Отмена", \
-                callback_data="cb_cancel"),
-            InlineKeyboardButton("Подтвердить удаление",
-                callback_data="cb_confirm_deletion"))
+
+    IKB = InlineKeyboardButton
+
+    cancel = IKB("Отмена", callback_data="cb_cancel")
+    c_del = IKB("Подтвердить удаление", callback_data="cb_confirm_deletion")
+
+    markup.add(cancel, c_del)
+
     return markup
 
 def gen_example_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    markup.add(InlineKeyboardButton("Отмемна", \
-                callback_data="cb_cancel"),
-            InlineKeyboardButton("Пример",
-                callback_data="cb_example_command"))
+
+    IKB = InlineKeyboardButton
+
+    cancel = IKB("Отмена", callback_data="cb_cancel")
+    example = IKB("Пример", callback_data="cb_example_command")
+
+    markup.add(cancel, example)
+
     return markup
