ae4de199365cdaea71709fa5a14875221097a611
Konstantin Runov <runebone1@gmail.com>
2022-04-20 15:24:46 +0300

    add funcs -> common; extract

--- bot_commands/common.py
+++ bot_commands/common.py
@@ -112,3 +112,73 @@ def get_phone_from_output_message_text(message_text):
     if (phone): phone = phone[0]
     else: phone = None
     return phone
+
+# =================================================
+
+def get_record_from_message_and_db(message, db):
+    # Don't interpret record and db.sample_record as the same thing
+    record = dict(db.sample_record)
+
+    name = get_name_from_message(message.text)
+    msg = remove_parsed_data_from_message(message.text, name)
+    record["name"] = name
+
+    date = get_date_from_message(msg)
+    msg = remove_parsed_data_from_message(msg, date)
+    date = normalize_date(date)
+    date = normal_date_to_usa_format(date)
+    record["date"] = date
+
+    if (message_has_at_sign(message)):
+        if (message_has_nickname(message)):
+            nickname = get_nickname_from_message(msg)
+            msg = remove_parsed_data_from_message(msg, nickname)
+            record["nickname"] = nickname[1:] # Store nickname without @
+        else:
+            raise InvalidNickname
+
+    if (message_has_phone(message)):
+        phone = get_phone_from_message(msg)
+        msg = remove_parsed_data_from_message(msg, phone)
+        phone = normalize_phone(phone)
+        record["phone"] = phone
+
+    return record
+
+def assert_message_is_not_command(message):
+    if (message_is_command(message)):
+        raise MessageIsCommand
+
+def assert_message_has_valid_length(message):
+    if (len(message.text) > Const.MAX_MESSAGE_LENGTH):
+        raise MessageTooLarge
+
+def assert_message_has_name_in_the_beginning(message):
+    name = get_name_from_message(message.text)
+    if (name == None):
+        raise NoNameInTheBeginning
+
+def assert_message_has_date(message):
+    date = get_date_from_message(message.text)
+    if (date == None):
+        raise NoDate
+
+def message_has_at_sign(message):
+    if (re.findall("@", message.text) == []):
+        return False
+    return True
+
+def message_has_nickname(message):
+    nickname = get_nickname_from_message(message.text)
+    if (nickname == None):
+        return False
+    return True
+
+def message_has_phone(message):
+    phone = get_phone_from_message(message.text)
+    if (phone == None):
+        return False
+    return True
+
+def remove_parsed_data_from_message(message_string, data):
+    return re.sub(r"{data}", "", message_string, count=1)
