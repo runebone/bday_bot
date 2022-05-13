120159b629913d28c360ebe8505012d15a9c1723
Konstantin Runov <runebone1@gmail.com>
2022-05-13 11:22:53 +0300

    handle editing by commands; not buttons

--- bot_commands/add.py
+++ bot_commands/add.py
@@ -22,7 +22,14 @@ def process_add_step(message, bot, db):
                 reply_markup=gen_default_actions_markup())
 
     except MessageIsCommand:
-        MessageIsCommand.add(message, default, bot, db)
+        if (message.text == "/example"):
+            # FIXME: DRY; same func as in main
+            bot.send_message(message.chat.id, "Пример:\n" + get_example())
+            bot.register_next_step_handler(message, process_add_step, bot, db)
+        elif (message.text == "/cancel"):
+            MessageIsCommand.cancel_add(message, default, bot, db)
+        else:
+            MessageIsCommand.add(message, default, bot, db)
     except MessageTooLarge:
         MessageTooLarge.default(message, process_add_step, bot, db)
     except NoNameInTheBeginning:
