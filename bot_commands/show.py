298945b5b877507b71fbc27304b63d38bff519c8
Konstantin Runov <runebone1@gmail.com>
2022-05-22 10:53:58 +0300

    show: show records in message (10 recs in each)

--- bot_commands/show.py
+++ bot_commands/show.py
@@ -6,9 +6,16 @@ def process_show_step(message, bot, db):
 
         records = db.get_user_records(message.chat.id)
 
+        msg = []
+
         for i in range(len(records)):
-            string = get_output_string(records[i], i)
-            bot.send_message(message.chat.id, string)
+            string = get_record_string(records[i], i)
+
+            msg.append(string)
+
+            if ((i + 1) > 0 and (i + 1) % 10 == 0 or i == (len(records) - 1)):
+                bot.send_message(message.chat.id, "\n".join(msg))
+                msg = []
 
         # Choose action
         bot.send_message(message.chat.id, BotText.CHOOSE_ACTION, \
